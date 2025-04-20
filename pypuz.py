9899a0a5732a13b756252c5613cd7e2b573eb60e
August Mint <augwik@gmail.com>
2025-04-20 03:11:59 +0200

    Readpuz now contains a Puzzle class

--- pypuz.py
+++ pypuz.py
@@ -4,9 +4,44 @@ import curses
 import readpuz as rp
 
 
+def draw(char):
+    pass
+
+
+def restorescreen():
+    curses.nocbreak()
+    curses.echo()
+    curses.endwin()
+
+
 if __name__ == "__main__":
     if len(sys.argv) != 2:
         print("Usage: python pypuz.py [.PUZ FILENAME]")
-    else:
-        cluedict = rp.readpuz(sys.argv[1])
-        print(cluedict)
+        exit()
+
+    # metadata = across, down,
+    puzzle = rp.Puzzle(sys.argv[1])
+    print(puzzle.cluedict)
+    for clue in puzzle.across:
+        print(clue)
+    for clue in puzzle.down:
+        print(clue)
+
+    # Create window object; size is whole screen
+    scrn = curses.initscr()
+    # turn off keystroke echo
+    curses.noecho()
+    # no need to press enter after key
+    curses.cbreak()
+    curses.start_color()
+    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_YELLOW)
+    scrn.clear()
+    while False:
+        # read character from keyboard
+        c = scrn.getch()
+        # ASCII int -> character
+        c = chr(c)
+        if c == 'q':
+            break
+        draw(c)
+    restorescreen()
--- readpuz.py
+++ readpuz.py
@@ -2,6 +2,35 @@ import sys
 import numpy as np
 
 
+class Puzzle:
+    def __init__(self, puz_file):
+        # TODO: add read and match and stuff
+        with open(puz_file, 'rb') as f:
+            self.fst_checksum = f.read(2)            # Overall file checksum
+            self.file_magic_b = f.read(12)         # NUL-terminated constant string
+            if self.file_magic_b != b"ACROSS&DOWN\x00":
+                print(f"""Error! Incorrect file magic. Got: {self.file_magic_b}
+                    Expected: ACROSS&DOWN""")
+                return 0
+            self.CIB_checksum = f.read(2)    # TODO: (defined later)
+            self.ML_checksums = f.read(4)    # Checksums, XOR-masked against a magic str
+            self.MH_checksums = f.read(4)    # Checksums, XOR-masked against a magic str
+            self.version_stri = f.read(4)    # Version string, e.g. 1.2
+            self.reserved_1C_ = f.read(2)    # In many files, uninitialized memory
+            self.scr_checksum = f.read(2)    # In scrambled puzzles, a solution checksum
+            _ = f.read(12)              # offset from 0x1F to 0x2C
+            self.width, self.height = f.read(2)   # width and height of board, one byte each
+            self.number_clues = f.read(2)    # the number of clues on this board
+            self.unknown_bits = f.read(2)    # a bitmask. operations unknown
+            self.is_scrambled = f.read(2)    # 0 for unscrambled, nonzero for scrambled
+            self.solution = np.array([list(f.read(self.width)) for _ in range(self.height)])
+            self.player_state = [f.read(self.width) for _ in range(self.height)]
+            rest = f.read()
+
+        self.title, self.author, self.copyrightstring, self.clues, self.notes = readrest(rest, self.number_clues)
+        self.cluedict, self.across, self.down = matchclues(self.clues, self.solution)
+
+
 def readrest(rest, number_clues):
     strings = rest.split(b'\x00')
     title = strings[0]
@@ -12,9 +41,11 @@ def readrest(rest, number_clues):
     return title, author, copyrightstring, clues, notes
 
 
-#   matchclues :: ([Bytes], np.array([[Bytes]])) -> {Coord -> (Clue, Clue)}
+#   matchclues :: ([Bytes], np.array([[Bytes]])) -> ({Coord -> (Clue, Clue)}, [String], [String])
 def matchclues(clues, solution):
     # iterate + keep track of cell above and to the left-linear time. optimal?
+    across = []
+    down = []
     height = solution.shape[0]
     width = solution.shape[1]
     cluedict = {(x, -1): ("$BLK", "$BLK") for x in range(width)}
@@ -26,18 +57,22 @@ def matchclues(clues, solution):
             if (solution[j, i] == 46):
                 cluedict[(i, j)] = ("$BLK", "$BLK")
             elif (cluedict[(i-1, j)][0] == "$BLK") and (cluedict[(i, j-1)][1] == "$BLK"):
+                across.append(clues[cluenumber])
+                down.append(clues[cluenumber+1])
                 cluedict[(i, j)] = (clues[cluenumber], clues[cluenumber + 1])
                 cluenumber += 2
             elif (cluedict[(i-1, j)][0] == "$BLK"):
+                across.append(clues[cluenumber])
                 cluedict[(i, j)] = (clues[cluenumber], cluedict[(i, j-1)][1])
                 cluenumber += 1
             elif (cluedict[(i, j-1)][1] == "$BLK"):
+                down.append(clues[cluenumber+1])
                 cluedict[(i, j)] = (cluedict[(i-1, j)][0], clues[cluenumber])
                 cluenumber += 1
             else:
                 cluedict[(i, j)] = (cluedict[(i-1, j)][0], cluedict[(i, j-1)][1])
 
-    return cluedict
+    return cluedict, across, down
 
 
 def readpuz(file, verbose=0):
@@ -64,7 +99,7 @@ def readpuz(file, verbose=0):
         rest = f.read()
 
     title, author, copyrightstring, clues, notes = readrest(rest, number_clues)
-    cluedict = matchclues(clues, solution)
+    cluedict, across, down = matchclues(clues, solution)
 
     if verbose == 1:
         print(f"{title=} {author=} {copyrightstring=} {clues=} {notes=}")
@@ -88,7 +123,7 @@ def readpuz(file, verbose=0):
             , unknown_bits
             , is_scrambled)
 
-    return cluedict
+    return cluedict, across, down, player_state
 
 
 if __name__ == "__main__":
