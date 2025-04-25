e76afc8dcf45391387c76406b800a83bea901955
August <augwik@gmail.com>
2025-04-25 14:24:37 +0200

    Puzzle layout and current clue are now displayed

--- pypuz.py
+++ pypuz.py
@@ -1,47 +1,74 @@
 import sys
 import curses
-# import numpy as np
 import readpuz as rp
 
 
-def draw(char):
-    pass
-
-
-def restorescreen():
-    curses.nocbreak()
-    curses.echo()
-    curses.endwin()
-
-
-if __name__ == "__main__":
+def main(stdscr):
     if len(sys.argv) != 2:
-        print("Usage: python pypuz.py [.PUZ FILENAME]")
-        exit()
+        stdscr.addstr("Usage: python pypuz.py [.PUZ FILENAME]")
+        stdscr.getkey()
+        return
 
-    # metadata = across, down,
     puzzle = rp.Puzzle(sys.argv[1])
-    print(puzzle.cluedict)
-    for clue in puzzle.across:
-        print(clue)
-    for clue in puzzle.down:
-        print(clue)
 
-    # Create window object; size is whole screen
-    scrn = curses.initscr()
-    # turn off keystroke echo
-    curses.noecho()
-    # no need to press enter after key
-    curses.cbreak()
-    curses.start_color()
-    curses.init_pair(1, curses.COLOR_BLACK, curses.COLOR_YELLOW)
-    scrn.clear()
-    while False:
+    stdscr.addstr(puzzle.title + b' - ' + puzzle.author + b'\n')
+    stdscr.addstr(puzzle.notes + b'\n')
+
+    bwidth = puzzle.width
+    bheight = puzzle.height
+    # wx = curses.COLS // 2 - bwidth # this can replace 1 in board = [...] , 1)
+    wy = (curses.LINES - bheight) // 2
+    board = curses.newwin(bheight, bwidth, wy, 1)
+    boarder = curses.newwin(bheight + 2, bwidth + 2, wy - 1, 0)
+    boarder.border()
+    belowboarder = wy + bheight + 1
+
+    # Draw player state
+    for i, row in enumerate(puzzle.player_state):
+        for j, b in enumerate(row):
+            if b == ord('.'):
+                board.addstr(i, j, 'X')
+    stdscr.refresh()
+    boarder.refresh()
+    board.refresh()
+
+    # Create window object; size is whole stdscreen
+    stdscr.keypad(True)
+
+    # Initializing variables for game loop
+    dir = 0  # 0 for across, 1 for down
+    while True:
         # read character from keyboard
-        c = scrn.getch()
+        c = stdscr.getkey()
         # ASCII int -> character
-        c = chr(c)
-        if c == 'q':
-            break
-        draw(c)
-    restorescreen()
+        y, x = board.getyx()
+        match c:
+            case 'q':
+                break
+            case "KEY_UP":
+                dir = 1
+                if y > 0:
+                    board.move(y-1, x)
+            case "KEY_DOWN":
+                dir = 1
+                if y < bheight - 1:
+                    board.move(y+1, x)
+            case "KEY_LEFT":
+                dir = 0
+                if x > 0:
+                    board.move(y, x-1)
+            case "KEY_RIGHT":
+                dir = 0
+                if x < bwidth - 1:
+                    board.move(y, x+1)
+
+        # Draw currclue
+        stdscr.addstr(belowboarder, 1, puzzle.cluedict[(x, y)][dir])
+
+        stdscr.refresh()
+        boarder.refresh()
+        board.refresh()
+    stdscr.keypad(False)
+
+
+curses.wrapper(main)
--- readpuz.py
+++ readpuz.py
@@ -1,10 +1,12 @@
 import sys
-import numpy as np
 
 
 class Puzzle:
     def __init__(self, puz_file):
-        # TODO: add read and match and stuff
+        self.read(puz_file)
+        self.matchclues()
+
+    def read(self, puz_file):
         with open(puz_file, 'rb') as f:
             self.fst_checksum = f.read(2)            # Overall file checksum
             self.file_magic_b = f.read(12)         # NUL-terminated constant string
@@ -23,111 +25,58 @@ class Puzzle:
             self.number_clues = f.read(2)    # the number of clues on this board
             self.unknown_bits = f.read(2)    # a bitmask. operations unknown
             self.is_scrambled = f.read(2)    # 0 for unscrambled, nonzero for scrambled
-            self.solution = np.array([list(f.read(self.width)) for _ in range(self.height)])
+            self.solution = [list(f.read(self.width)) for _ in range(self.height)]
             self.player_state = [f.read(self.width) for _ in range(self.height)]
             rest = f.read()
+        self.readrest(rest)
 
-        self.title, self.author, self.copyrightstring, self.clues, self.notes = readrest(rest, self.number_clues)
-        self.cluedict, self.across, self.down = matchclues(self.clues, self.solution)
+    def readrest(self, rest):
+        strings = rest.split(b'\x00')
+        self.title = strings[0]
+        self.author = strings[1]
+        self.copyrightstring = strings[2]
+        self.clues = strings[3:3 + int(self.number_clues[0])]
+        self.notes = strings[-1]
+        return self
 
-
-def readrest(rest, number_clues):
-    strings = rest.split(b'\x00')
-    title = strings[0]
-    author = strings[1]
-    copyrightstring = strings[2]
-    clues = strings[3:3 + int(number_clues[0])]
-    notes = strings[-1]
-    return title, author, copyrightstring, clues, notes
-
-
-#   matchclues :: ([Bytes], np.array([[Bytes]])) -> ({Coord -> (Clue, Clue)}, [String], [String])
-def matchclues(clues, solution):
-    # iterate + keep track of cell above and to the left-linear time. optimal?
-    across = []
-    down = []
-    height = solution.shape[0]
-    width = solution.shape[1]
-    cluedict = {(x, -1): ("$BLK", "$BLK") for x in range(width)}
-    for y in range(height):
-        cluedict[(-1, y)] = ("$BLK", "$BLK")
-    cluenumber = 0
-    for j in range(height):
-        for i in range(width):
-            if (solution[j, i] == 46):
-                cluedict[(i, j)] = ("$BLK", "$BLK")
-            elif (cluedict[(i-1, j)][0] == "$BLK") and (cluedict[(i, j-1)][1] == "$BLK"):
-                across.append(clues[cluenumber])
-                down.append(clues[cluenumber+1])
-                cluedict[(i, j)] = (clues[cluenumber], clues[cluenumber + 1])
-                cluenumber += 2
-            elif (cluedict[(i-1, j)][0] == "$BLK"):
-                across.append(clues[cluenumber])
-                cluedict[(i, j)] = (clues[cluenumber], cluedict[(i, j-1)][1])
-                cluenumber += 1
-            elif (cluedict[(i, j-1)][1] == "$BLK"):
-                down.append(clues[cluenumber+1])
-                cluedict[(i, j)] = (cluedict[(i-1, j)][0], clues[cluenumber])
-                cluenumber += 1
-            else:
-                cluedict[(i, j)] = (cluedict[(i-1, j)][0], cluedict[(i, j-1)][1])
-
-    return cluedict, across, down
-
-
-def readpuz(file, verbose=0):
-    with open(file, 'rb') as f:
-        fst_checksum = f.read(2)            # Overall file checksum
-        file_magic_b = f.read(12)         # NUL-terminated constant string
-        if file_magic_b != b"ACROSS&DOWN\x00":
-            print(f"""Error! Incorrect file magic. Got: {file_magic_b}
-                Expected: ACROSS&DOWN""")
-            return 0
-        CIB_checksum = f.read(2)    # TODO: (defined later)
-        ML_checksums = f.read(4)    # Checksums, XOR-masked against a magic str
-        MH_checksums = f.read(4)    # Checksums, XOR-masked against a magic str
-        version_stri = f.read(4)    # Version string, e.g. 1.2
-        reserved_1C_ = f.read(2)    # In many files, uninitialized memory
-        scr_checksum = f.read(2)    # In scrambled puzzles, a solution checksum
-        _ = f.read(12)              # offset from 0x1F to 0x2C
-        width, height = f.read(2)   # width and height of board, one byte each
-        number_clues = f.read(2)    # the number of clues on this board
-        unknown_bits = f.read(2)    # a bitmask. operations unknown
-        is_scrambled = f.read(2)    # 0 for unscrambled, nonzero for scrambled
-        solution = np.array([list(f.read(width)) for _ in range(height)])
-        player_state = [f.read(width) for _ in range(height)]
-        rest = f.read()
-
-    title, author, copyrightstring, clues, notes = readrest(rest, number_clues)
-    cluedict, across, down = matchclues(clues, solution)
-
-    if verbose == 1:
-        print(f"{title=} {author=} {copyrightstring=} {clues=} {notes=}")
-        print(solution)
+    #   matchclues :: ([Bytes], np.array([[Bytes]])) -> ({Coord -> (Clue, Clue)}, [String], [String])
+    def matchclues(self):
+        # iterate + keep track of cell above and to the left-linear time. optimal?
+        across = []
+        down = []
+        height = len(self.solution)
+        width = len(self.solution[0])
+        cluedict = {(x, -1): ("$BLK", "$BLK") for x in range(width)}
         for y in range(height):
-            for x in range(width):
-                print(f'{x}, {y}, {cluedict[(x, y)]}')
-    if verbose == 2:
-        for row in player_state:
-            print(row)
-        print(file_magic_b, version_stri, width, height, number_clues)
-        print(
-            fst_checksum
-            , CIB_checksum
-            , ML_checksums
-            , MH_checksums
-            , version_stri
-            , reserved_1C_
-            , scr_checksum
-            , number_clues
-            , unknown_bits
-            , is_scrambled)
+            cluedict[(-1, y)] = ("$BLK", "$BLK")
+        cluenumber = 0
+        for j in range(height):
+            for i in range(width):
+                if (self.solution[j][i] == 46):
+                    cluedict[(i, j)] = ("$BLK", "$BLK")
+                elif (cluedict[(i-1, j)][0] == "$BLK") and (cluedict[(i, j-1)][1] == "$BLK"):
+                    across.append(self.clues[cluenumber])
+                    down.append(self.clues[cluenumber+1])
+                    cluedict[(i, j)] = (self.clues[cluenumber], self.clues[cluenumber + 1])
+                    cluenumber += 2
+                elif (cluedict[(i-1, j)][0] == "$BLK"):
+                    across.append(self.clues[cluenumber])
+                    cluedict[(i, j)] = (self.clues[cluenumber], cluedict[(i, j-1)][1])
+                    cluenumber += 1
+                elif (cluedict[(i, j-1)][1] == "$BLK"):
+                    down.append(self.clues[cluenumber+1])
+                    cluedict[(i, j)] = (cluedict[(i-1, j)][0], self.clues[cluenumber])
+                    cluenumber += 1
+                else:
+                    cluedict[(i, j)] = (cluedict[(i-1, j)][0], cluedict[(i, j-1)][1])
 
-    return cluedict, across, down, player_state
+        self.cluedict = cluedict
+        self.across = across
+        self.down = down
 
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
+    if sys.argv[1][-4:] != ".puz":
         print("Usage: python readpuz.py [.PUZ FILENAME]")
     else:
-        readpuz(sys.argv[1])
+        Puzzle(sys.argv[1])
