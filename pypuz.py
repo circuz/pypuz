6f3454de9fe52edb51e7281930f38fa525f49771
August Mint <augwik@gmail.com>
2025-04-27 15:27:13 +0200

    Clue highlighting works now!

--- pypuz.py
+++ pypuz.py
@@ -3,6 +3,42 @@ import curses
 import readpuz as rp
 
 
+def highlight(scr: curses.window, puz: rp.Puzzle, y, x, dir):
+    if puz.player_state[y][x] == ord('.'):
+        return
+    elif dir == 0:
+        i = 1
+        j = 0
+        while (i < (puz.width-x)) and (puz.player_state[y+j][x+i] != ord('.')):
+            c = chr(scr.inch(y+j, x+i))
+            scr.delch(y+j, x+i)
+            scr.insch(y+j, x+i, c, curses.A_REVERSE)  # fst
+            i += 1
+        i = -1
+        j = 0
+        while (i > -x-1) and (puz.player_state[y+j][x+i] != ord('.')):
+            c = chr(scr.inch(y+j, x+i))
+            scr.delch(y+j, x+i)
+            scr.insch(y+j, x+i, c, curses.A_REVERSE)  # snd
+            i -= 1
+    else:
+        i = 0
+        j = 1
+        while (j < (puz.height-y)) and (puz.player_state[y+j][x+i] != ord('.')):
+            c = chr(scr.inch(y+j, x+i))
+            scr.delch(y+j, x+i)
+            scr.insch(y+j, x+i, c, curses.A_REVERSE)  # fst
+            j += 1
+        i = 0
+        j = -1
+        while (j > -y-1) and (puz.player_state[y+j][x+i] != ord('.')):
+            c = chr(scr.inch(y+j, x+i))
+            scr.delch(y+j, x+i)
+            scr.insch(y+j, x+i, c, curses.A_REVERSE)  # snd
+            j -= 1
+    return
+
+
 def main(stdscr):
     if len(sys.argv) != 2:
         stdscr.addstr("Usage: python pypuz.py [.PUZ FILENAME]")
@@ -20,14 +56,17 @@ def main(stdscr):
     wy = (curses.LINES - bheight) // 2
     board = curses.newwin(bheight, bwidth, wy, 1)
     boarder = curses.newwin(bheight + 2, bwidth + 2, wy - 1, 0)
-    boarder.border()
+    boarder.border(0, 0, 0, 0, 0, 0, 0, 0)
     belowboarder = wy + bheight + 1
 
     # Draw player state
     for i, row in enumerate(puzzle.player_state):
         for j, b in enumerate(row):
             if b == ord('.'):
-                board.addstr(i, j, '#')
+                board.insstr(i, j, u'\u2588')
+            else:
+                board.insstr(i, j, chr(b))
+    board.move(8, 8)
     stdscr.refresh()
     boarder.refresh()
     board.refresh()
@@ -38,13 +77,22 @@ def main(stdscr):
     # Initializing variables for game loop
     dir = 0  # 0 for across, 1 for down
     while True:
+        y, x = board.getyx()
+        for i, row in enumerate(puzzle.player_state):
+            for j, b in enumerate(row):
+                if b == ord('.'):
+                    board.insstr(i, j, u'\u2588')
+                else:
+                    board.insstr(i, j, chr(b))
+        board.move(y, x)
         # read character from keyboard
         c = stdscr.getkey()
         # ASCII int -> character
-        y, x = board.getyx()
         match c:
             case "KEY_BACKSPACE":
                 break
+            case " ":
+                dir = 1 - dir
             case "KEY_UP":
                 dir = 1
                 if y > 0:
@@ -61,6 +109,10 @@ def main(stdscr):
                 dir = 0
                 if x < bwidth - 1:
                     board.move(y, x+1)
+
+        y, x = board.getyx()
+        highlight(board, puzzle, y, x, dir)
+        board.move(y, x)
 
         # Draw currclue
         stdscr.addstr(belowboarder, 1, puzzle.cluedict[(x, y)][dir])
